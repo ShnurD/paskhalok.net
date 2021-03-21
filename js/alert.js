c800d777d9213382454fe65b28acc3d16f3946ab
Shnur_D <45401533+ShnurD@users.noreply.github.com>
2021-03-21 19:34:26 +0300

    Add files via upload

--- js/alert.js
+++ js/alert.js
@@ -1,1 +1,23 @@
-<!-- d0 9f d0 b0 d1 80 d0 be d0 bb d1 8c 3a 20 32 32 38 37 -->
+  if (confirm("Давай проверим, дурачок ли ты?)")) { 
+      if (confirm("Жми ОК или ОТМЕНА!")){
+          alert("А ты крутой!");
+      } else{
+          alert("Я тебя сюда не посылал");
+            if (confirm("Ты думаешь я тебя не переиграю?")){
+                if (confirm("Ну получается посылал. Или нет?")){
+                    document.location.replace("https://yandex.ru/search/?text=%D0%BA%D0%B0%D0%BA%20%D0%BF%D0%B5%D1%80%D0%B5%D1%81%D1%82%D0%B0%D1%82%D1%8C%20%D0%B4%D0%BE%D0%B2%D0%B5%D1%80%D1%8F%D1%82%D1%8C%20%D0%BB%D1%8E%D0%B4%D1%8F%D0%BC%3F&lr=10725");
+                } else{
+                    if (confirm("Ты уверен в себе?")){
+                        alert("Нельзя быть таким самоуверенным");
+                        document.location.replace("https://yandex.ru/images/search?pos=20&from=tabbar&img_url=https%3A%2F%2Fsun1-19.userapi.com%2FNN8a8sIXs1MDH0fW017UR-iJPuFvjn8rXfW7iA%2FEMOrsM59bUc.jpg&text=%D1%82%D1%8B%20%D0%B4%D1%83%D0%BC%D0%B0%D0%B5%D1%88%D1%8C%20%D1%8F%20%D1%82%D0%B5%D0%B1%D1%8F%20%D0%BD%D0%B5%20%D0%BF%D0%B5%D1%80%D0%B5%D0%B8%D0%B3%D1%80%D0%B0%D1%8E&rpt=simage");
+                    } else{
+                            alert("А стоило бы.");
+                    } 
+            }} else 
+                alert('Пасхалка близко.');
+                document.location.replace("https://shnurd.github.io/paskhalok.net/maestro.html");
+            
+                
+}} else { 
+alert ("Может ты и не дурачок?\nЕще посмотрим...");  
+}
